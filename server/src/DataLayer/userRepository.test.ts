ba2760133a9395d1a9bf7caa4dd84ebdd16acad5
MThornell <mathew.thornell@quantumIT.com.au>
2019-05-18 10:39:24 +0800

    Jest Install and Setup

---
+++ server/src/DataLayer/userRepository.test.ts
@@ -0,0 +1,5 @@
+import 'jest';
+
+describe('User Repository', () => {
+    test('does a thing', () => {});
+});
--- server/webpack.config.js
+++ server/webpack.config.js
@@ -2,24 +2,24 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  entry: './index.ts',
-  devtool: 'inline-source-map',
-  target: 'node',
-  externals: [ nodeExternals() ],
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build'),
-  }
+    entry: './index.ts',
+    devtool: 'inline-source-map',
+    target: 'node',
+    externals: [nodeExternals()],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: ['/node_modules/']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build')
+    }
 };
