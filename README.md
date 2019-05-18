6d0f85f4f943ebbfc22efa067d7e2d4e1583a1c3
MThornell <mathew.thornell@quantumIT.com.au>
2019-05-18 14:25:52 +0800

    User Repository Tests

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
-
 # NodeJS Skeleton Project
+
 A ready-to-go skeleton project backend server for NodeJS.
 
 ## Complete with
-- Webpack Config
-- Typescript + Typescript Config
-- Web Server (ExpressJs)
-- IOC Containers (InversifyJs)
-- ORM/Postgres (SequelizeJs)
-- JWT Authentication (JsonWebToken)
 
+-   Webpack Config
+-   Typescript + Typescript Config
+-   Web Server (ExpressJs)
+-   IOC Containers (InversifyJs)
+-   ORM/Postgres (SequelizeJs)
+-   JWT Authentication (JsonWebToken)
+-   Testing (Jest)
--- server/src/DataLayer/userRepository.test.ts
+++ server/src/DataLayer/userRepository.test.ts
@@ -1,5 +1,71 @@
+import 'reflect-metadata';
 import 'jest';
 
+import { IUserRepository, UserRepository } from './userRepository';
+import { User } from 'Models/user';
+
+const mockUser: User = {
+    userid: 666,
+    username: 'user',
+    passwordHash: 'PasswordHash',
+    passwordSalt: 'PasswordSalt'
+};
+
+function createMockQueryResult(user: User) {
+    return {
+        dataValues: {
+            ...user
+        }
+    };
+}
+
 describe('User Repository', () => {
-    test('does a thing', () => {});
+    let userRepository: IUserRepository;
+    let mockUserContext: any;
+
+    beforeEach(() => {
+        mockUserContext = {
+            findAll: jest.fn().mockReturnValue(Promise.resolve([createMockQueryResult(mockUser)])),
+            upsert: jest.fn().mockReturnValue(Promise.resolve([createMockQueryResult(mockUser)])),
+            update: jest.fn().mockReturnValue(Promise.resolve([0, [createMockQueryResult(mockUser)]])),
+            destroy: jest.fn().mockReturnValue(Promise.resolve(1))
+        };
+
+        userRepository = new UserRepository(mockUserContext);
+    });
+
+    test('Gets Users', (done) => {
+        userRepository.get().then((result) => {
+            expect(mockUserContext.findAll).toBeCalled();
+            expect(result.length).toBeGreaterThan(0);
+            expect(result[0].userid).toBe(666);
+            done();
+        });
+    });
+
+    test('Inserts A User', (done) => {
+        userRepository.insert(mockUser).then((result) => {
+            expect(mockUserContext.upsert).toBeCalled();
+            expect(result.length).toBeGreaterThan(0);
+            expect(result[0].userid).toBe(666);
+            done();
+        });
+    });
+
+    test('Updates A User', (done) => {
+        userRepository.update(mockUser).then((result) => {
+            expect(mockUserContext.update).toBeCalled();
+            expect(result.length).toBeGreaterThan(0);
+            expect(result[0].userid).toBe(666);
+            done();
+        });
+    });
+
+    test('Deletes A User', (done) => {
+        userRepository.delete(1).then((result) => {
+            expect(mockUserContext.destroy).toBeCalled();
+            expect(result).toBeTruthy();
+            done();
+        });
+    });
 });
